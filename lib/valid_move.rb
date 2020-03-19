47ca9613f9a3fefa363a54842954ad36ff58c956
Thomas John Helin <thomashelin@yahoo.com>
2020-03-19 18:14:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
  if index.between?(0, 8)
    return true
- if position_taken?(board, index)
+ if index.between?(0, 8) && position_taken?(board, index)
    return false
  end
 end
